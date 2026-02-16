5aeabe2330994d1b5fd02a4a9f39aa871d877de6
java-VIP-Ben <2smallgarage@gmail.com>
2026-02-15 23:28:40 -0600

    working dynamic page

--- CIS325_Assignment1/script.js
+++ CIS325_Assignment1/script.js
@@ -5,10 +5,29 @@ const button = document.getElementById('myButton');
 const paragraph = document.getElementById('demo');
 
 // Add an event listener for a click
-button.addEventListener('click', () => {
-  alert("Annoying Dropdown!");
+if(button && paragraph) {
+  button.addEventListener('click', () => {
+    alert("Annoying Dropdown!");
     // Change the paragraph's text and the button's color when clicked
     paragraph.textContent = 'Thank you for visiting my webpage!';
     button.style.backgroundColor =
-        button.style.backgroundColor === 'red' ? 'blue' : 'red';
-});
+      button.style.backgroundColor === 'red' ? 'blue' : 'red';
+  });
+}
+
+const listButton = document.getElementById('listButton');
+const ul = document.getElementById('myList');
+const itemInput = document.getElementById('itemInput');
+
+if(listButton && ul && itemInput) {
+  // adding list elements to dynamic page
+  listButton.addEventListener('click', () => {
+    const newItemText = itemInput.value;
+    if(newItemText !== '') {
+      const newLi = document.createElement('li');
+      newLi.textContent = newItemText;
+      ul.appendChild(newLi);
+      itemInput.value = '';
+    }
+  });
+}
