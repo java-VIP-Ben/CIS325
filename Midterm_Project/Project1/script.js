ee5cc0cd02fa61fb9f36713d06e7675895c784f6
java-VIP-Ben <2smallgarage@gmail.com>
2026-04-08 12:59:24 -0500

    refresh

--- Midterm_Project/Project1/script.js
+++
@@ -1,6 +0,0 @@
-// Benjamin Lukens CIS325 Midterm Project 1 Script.js
-
-// opening default browser for email page
-document.getElementById('contactLink').addEventListener('click', function() {
-  window.location.href = "mailto:jowen22@murraystate.edu";
-});
